6eb6d554bfb8a147652cdd1d9bf6ea069e549bfa
panchal-yash <yash.panchal@percona.com>
2022-08-26 18:38:17 +0530

    add conditionals 10

--- pxb/jenkins/periodic-product-check-for-new-releases-build.groovy
+++ pxb/jenkins/periodic-product-check-for-new-releases-build.groovy
@@ -30,8 +30,10 @@ void popArtifactFile(String FILE_NAME) {
 
 void checkArtifactFile(String FILE_NAME) {
     withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: '24e68886-c552-4033-8503-ed85bbaa31f3', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+            S3_PATH="s3://product-release-check"
+    
     sh(script: """
-            S3_PATH=s3://product-release-check
+
             exists=\$(aws s3 ls ${S3_PATH}/${FILE_NAME})
             if [ -z "$exists" ]; then
                 echo "0"
