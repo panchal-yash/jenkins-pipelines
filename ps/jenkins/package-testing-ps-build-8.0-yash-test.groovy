a1edfdf69e2aa4fbf215092175c64255abc0248b
panchal-yash <yash.panchal@percona.com>
2022-09-12 15:40:40 +0530

    add branch

--- ps/jenkins/package-testing-ps-build-8.0-yash-test.groovy
+++ ps/jenkins/package-testing-ps-build-8.0-yash-test.groovy
@@ -98,7 +98,7 @@ void runPlaybook(String action_to_test) {
     setup_package_tests()
 
     sh '''
-        git clone --depth 1 -b PS-8347-add-check-for-warnings-PS-test https://github.com/panchal-yash/package-testing
+        git clone --depth 1 -b PS-8392-install-mysql-shell-backwards-compatibility-fix-env-issue https://github.com/panchal-yash/package-testing
     '''
 
     sh """
