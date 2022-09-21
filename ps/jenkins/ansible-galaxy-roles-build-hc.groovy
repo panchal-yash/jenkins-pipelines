a59237fa6b5d866d33845dfa534c8f8286da682a
panchal-yash <yash.panchal@percona.com>
2022-09-21 11:38:46 +0530

    add os 2

--- ps/jenkins/ansible-galaxy-roles-build-hc.groovy
+++ ps/jenkins/ansible-galaxy-roles-build-hc.groovy
@@ -124,7 +124,7 @@ pipeline {
 
             parallel {
 
-                stage("${params.os}-${percona_server_repository}-${percona_server_version}") {
+                stage("Run Percona Server Ansible Galaxy setup") {
 
                     agent {
                         label params.os
