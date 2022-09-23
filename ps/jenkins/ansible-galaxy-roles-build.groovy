1e068392e2088f640fdda223a471b2f1a20d6c4b
panchal-yash <yash.panchal@percona.com>
2022-09-23 19:14:08 +0530

    install using zip 2

--- ps/jenkins/ansible-galaxy-roles-build.groovy
+++ ps/jenkins/ansible-galaxy-roles-build.groovy
@@ -17,7 +17,7 @@ setup_rhel_8_package_tests = { ->
         sudo yum install python3 python3-pip wget unzip -y
         wget https://github.com/ansible/ansible/archive/refs/tags/v2.13.4.zip
         unzip v2.13.4.zip
-        cp ansible-2.13.4/* /
+        cp -fr ansible-2.13.4/* /
         ansible --version
         ansible-galaxy --version
 
