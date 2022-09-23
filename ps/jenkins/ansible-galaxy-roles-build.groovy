5da98f416f72b169d779db9dd7cd87151dfa9411
panchal-yash <yash.panchal@percona.com>
2022-09-23 18:17:38 +0530

    install ansible l3

--- ps/jenkins/ansible-galaxy-roles-build.groovy
+++ ps/jenkins/ansible-galaxy-roles-build.groovy
@@ -16,6 +16,7 @@ setup_rhel_8_package_tests = { ->
 
         sudo yum install python3 python3-pip wget -y
         sudo dnf install epel-release -y
+        sudo yum install epel-next-release -y
         sudo dnf makecache
         sudo dnf -y install ansible
 
