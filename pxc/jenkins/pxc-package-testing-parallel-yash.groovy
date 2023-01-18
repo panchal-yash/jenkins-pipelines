478e95db2c67e45609c6e4700b2903121eee2260
panchal-yash <yash.panchal@percona.com>
2023-01-19 00:25:08 +0530

    Install molecule reverted to

--- pxc/jenkins/pxc-package-testing-parallel-yash.groovy
+++ pxc/jenkins/pxc-package-testing-parallel-yash.groovy
@@ -153,7 +153,6 @@ pipeline {
                     }                
                 }   
                 echo "${JENWORKSPACE}"
-                installMolecule()
                 installDependencies()
             }
         }
