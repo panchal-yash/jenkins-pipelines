bb25c4766b7667adc045613cf8e6c58450da9739
panchal-yash <yash.panchal@percona.com>
2023-01-19 21:16:59 +0530

    Destroyy

--- vars/moleculepxcParallelDestroy.groovy
+++ vars/moleculepxcParallelDestroy.groovy
@@ -3,23 +3,9 @@ def call(operatingSystems, product_to_test) {
   operatingSystems.each { os ->
    tests["${os}"] =  {
         stage("${os}") {
-          
-          echo "DESTROYING"
-          sh """
-            cd package-testing/molecule/pxc
 
-            cd ${product_to_test}-bootstrap
-            export INSTANCE_PRIVATE_IP=\${BOOTSTRAP_INSTANCE_PRIVATE_IP}
-            export INSTANCE_PUBLIC_IP=\${BOOTSTRAP_INSTANCE_PUBLIC_IP}            
-            molecule destroy -s ${os}
-            cd -
+          moleculepxcRunMoleculeAction("destroy", params.product_to_test, "${os}", "install", params.test_repo, "yes")
 
-            cd ${product_to_test}-common
-            export INSTANCE_PRIVATE_IP=\${COMMON_INSTANCE_PRIVATE_IP}
-            export INSTANCE_PUBLIC_IP=\${COMMON_INSTANCE_PUBLIC_IP}        
-            molecule destroy -s ${os}
-            cd -
-          """
         }
       }
     }
