2fca1ea3cf7ea4cb7478d7a032635b18e37a0c12
panchal-yash <yash.panchal@percona.com>
2023-01-19 19:22:20 +0530

    Test with 2 os onluy

--- pxc/jenkins/pxc-package-testing-parallel-yash.groovy
+++ pxc/jenkins/pxc-package-testing-parallel-yash.groovy
@@ -5,14 +5,7 @@ library changelog: false, identifier: 'lib@wip-pxc-package-testing-upgrade-test'
 
 List all_nodes = [
                 'ubuntu-jammy',
-                'ubuntu-focal',
-                'ubuntu-bionic',
-                'debian-11',
-                'debian-10',
-                'centos-7',
-                'ol-8',
-                'ol-9',
-                'min-amazon-2'
+                'ubuntu-focal'
 ]
 
 product_to_test = params.product_to_test
