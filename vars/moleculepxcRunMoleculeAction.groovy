de299c605676da233f8baba3b905910bff4e2f87
panchal-yash <yash.panchal@percona.com>
2023-01-20 16:53:57 +0530

    -v 3

--- vars/moleculepxcRunMoleculeAction.groovy
+++ vars/moleculepxcRunMoleculeAction.groovy
@@ -55,13 +55,13 @@ void call(String action, String product_to_test, String scenario, String test_ty
             cd ${product_to_test}-bootstrap
             echo "INSTANCE_PRIVATE_IP: ${BOOTSTRAP_INSTANCE_PRIVATE_IP}" > envfile
             echo "INSTANCE_PUBLIC_IP: ${BOOTSTRAP_INSTANCE_PUBLIC_IP}" >> envfile
-            molecule -e envfile ${action} -vvv -s ${scenario}
+            molecule -v 3 -e envfile ${action} -s ${scenario}
             cd -
 
             cd ${product_to_test}-common
             echo "INSTANCE_PRIVATE_IP: ${COMMON_INSTANCE_PRIVATE_IP}" > envfile_common
             echo "INSTANCE_PUBLIC_IP: ${COMMON_INSTANCE_PUBLIC_IP} >> envfile_common
-            molecule -vvv -e envfile_common ${action} -s ${scenario}
+            molecule -v 3 -e envfile_common ${action} -s ${scenario}
             cd -
         """
 
