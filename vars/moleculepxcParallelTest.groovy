961139b989c3f5f060872cdbcc770d545e996bf0
panchal-yash <yash.panchal@percona.com>
2023-01-23 14:23:20 +0530

    remove extra =

--- vars/moleculepxcParallelTest.groovy
+++ vars/moleculepxcParallelTest.groovy
@@ -4,13 +4,13 @@ def call(operatingSystems, String test_type) {
    tests["${os}"] =  {
         stage("${os}") {
 
-          if (test_type == "install" || test_type === "install_and_upgrade"){
+          if (test_type == "install" || test_type == "install_and_upgrade"){
 
             echo "INSTALLING"
             moleculepxcPackageTestsINSTALL("${os}")
 
           }
-          else if (test_type == "upgrade" || test_type === "install_and_upgrade"){
+          else if (test_type == "upgrade" || test_type == "install_and_upgrade"){
 
             echo "UPGRADING"
             moleculepxcPackageTestsUPGRADE("${os}")
