04baa4fc47319350f3d660b9272f281347851854
panchal-yash <yash.panchal@percona.com>
2023-01-23 22:44:35 +0530

    remove some ;

--- vars/moleculepxcRunMoleculeAction.groovy
+++ vars/moleculepxcRunMoleculeAction.groovy
@@ -40,7 +40,7 @@ void call(String action, String product_to_test, String scenario, String test_ty
                     echo 'INSTANCE_PUBLIC_IP: "${BOOTSTRAP_INSTANCE_PUBLIC_IP}"' >> ${WORKSPACE}/package-testing/molecule/pxc/${product_to_test}-bootstrap/molecule/${scenario}/${test_type}/envfile
  
                     
-                    if [[ ${test_type} = "install"]];
+                    if [[ ${test_type} = "install"]]
                     then
                         echo 'install_repo: "${test_repo}"' >> ${WORKSPACE}/package-testing/molecule/pxc/${product_to_test}-bootstrap/molecule/${scenario}/${test_type}/envfile
                         echo 'check_version: "${version_check}"' >> ${WORKSPACE}/package-testing/molecule/pxc/${product_to_test}-bootstrap/molecule/${scenario}/${test_type}/envfile
@@ -60,7 +60,7 @@ void call(String action, String product_to_test, String scenario, String test_ty
                     echo "INSTANCE_PRIVATE_IP: "${COMMON_INSTANCE_PRIVATE_IP}"" > ${WORKSPACE}/package-testing/molecule/pxc/${product_to_test}-common/molecule/${scenario}/${test_type}/envfile
                     echo "INSTANCE_PUBLIC_IP: "${COMMON_INSTANCE_PUBLIC_IP}"" >> ${WORKSPACE}/package-testing/molecule/pxc/${product_to_test}-common/molecule/${scenario}/${test_type}/envfile
  
-                    if [[ ${test_type} = "install"]];
+                    if [[ ${test_type} = "install"]]
                     then
                         echo 'install_repo: "${test_repo}"' >> ${WORKSPACE}/package-testing/molecule/pxc/${product_to_test}-common/molecule/${scenario}/${test_type}/envfile
                         echo 'check_version: "${version_check}"' >> ${WORKSPACE}/package-testing/molecule/pxc/${product_to_test}-common/molecule/${scenario}/${test_type}/envfile
