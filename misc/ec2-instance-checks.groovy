a3e1ea0997be750c59bdbeb8c8ad4002ad53e419
panchal-yash <yash.panchal@percona.com>
2023-06-13 16:18:50 +0530

    use the class

--- misc/ec2-instance-checks.groovy
+++ misc/ec2-instance-checks.groovy
@@ -2,19 +2,6 @@ library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
-    def awsCredentials = [
-        sshUserPrivateKey(
-            credentialsId: 'MOLECULE_AWS_PRIVATE_KEY',
-            keyFileVariable: 'MOLECULE_AWS_PRIVATE_KEY',
-            passphraseVariable: '',
-            usernameVariable: ''
-        ),
-        aws(
-            accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-            credentialsId: '5d78d9c7-2188-4b16-8e31-4d5782c6ceaa',
-            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-        )
-    ]
 
 pipeline {
     agent {
@@ -25,7 +12,6 @@ pipeline {
         skipDefaultCheckout()
     }
 
-
     stages {
         stage("Cleanup Workspace") {
 
@@ -34,18 +20,50 @@ pipeline {
     
                 echo "Check"
 
-                withCredentials(awsCredentials) {
+                    echo "${JENKINS_HOME}"
+
                     sh """
-                        echo JELO
-                        
-                        wget https://raw.githubusercontent.com/panchal-yash/package-testing/ec-instance-checks/scripts/check-ec2-instances.sh
-
-                        chmod +x check-ec2-instances.sh
-
-                        ./check-ec2-instances.sh us-east-1 3 | jq -r '.[] | select(length > 0)'
-
+                    set +xe 
+                    sudo yum install jq -y
+                    cd ${JENKINS_HOME}
+                    wget https://raw.githubusercontent.com/panchal-yash/package-testing/ec-instance-checks/scripts/check-ec2-instances.sh
+                    chmod +x check-ec2-instances.sh
                     """
-                }
+             script{
+
+
+                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'c42456e5-c28d-4962-b32c-b75d161bff27', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+
+                        env.DAYS = "3"
+
+
+
+                        //env.CHECK = sh(returnStdout: true).trim()
+
+sh """
+set +xe
+#!/bin/bash
+awsRegions=("us-west-1" "us-west-2")
+#
+days="3"
+for region in "\$awsRegions[@]"
+do
+
+
+echo "----------------"$region"----------------------"
+        ./check-ec2-instances.sh "$region" "$days"
+echo "----------------------------------------------"
+done
+
+                        """
+
+
+                    }
+
+                        echo "Print the output"
+                        echo "${env.CHECK}"
+                        echo "${env.DAYS}"
+             }
 
             }
         }
@@ -57,18 +75,17 @@ pipeline {
 
         always {
              catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE'){
-                archiveArtifacts artifacts: 'PXC/**/*.tar.gz' , followSymlinks: false
+                script{
+                    slackSend channel: '#dev-server-qa', color: '#DEFF13', message: """
+                    
+${env.CHECK}
+                    
+                    """
+                
+                
+                }
              }
         }
-
-        unstable {
-                slackSend channel: '#dev-server-qa', color: '#DEFF13', message: "[${env.JOB_NAME}]: Failed during the Package testing (Unstable Build) [${env.BUILD_URL}] Parameters: product_to_test: ${params.product_to_test} , node_to_test: ${params.node_to_test} , test_repo: ${params.test_repo}, test_type: ${params.test_type}"
-        }
-
-        failure {
-                slackSend channel: '#dev-server-qa', color: '#FF0000', message: "[${env.JOB_NAME}]: Failed during the Package testing (Build Failed) [${env.BUILD_URL}] Parameters: product_to_test: ${params.product_to_test} , node_to_test: ${params.node_to_test} , test_repo: ${params.test_repo}, test_type: ${params.test_type}"
-        }
-
 
     }
 
