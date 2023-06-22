9182ea69f4ff5ecf8615cf4c255835d8dd5849b7
panchal-yash <yash.panchal@percona.com>
2023-06-22 19:35:46 +0530

    add github-token-test

---
+++ misc/github-token-test.groovy
@@ -0,0 +1,54 @@
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+]) _
+
+pipeline {
+    agent {
+        label 'micro-amazon'
+    }
+    options {
+        skipDefaultCheckout()
+    }
+
+    stages {
+
+
+        stage("Cleanup Workspace") {
+            steps {                
+                sh "sudo rm -rf ${WORKSPACE}/*"
+            }
+        }
+
+        stage("Checks") {
+
+            
+
+            steps {              
+             script{
+
+
+                    withCredentials([string(credentialsId: 'PXC_GITHUB_API_TOKEN', variable: 'TOKEN')]) {
+
+                    sh """
+ 
+                    git clone https://jenkins-pxc-cd:$TOKEN@github.com/Percona-QA/package-testing.git
+
+                    cd package-testing
+
+                    cat .git/config
+
+                    """
+
+                    }
+             }
+
+            }
+        }
+        
+    }
+
+
+
+
+    }
