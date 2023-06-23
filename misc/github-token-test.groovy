a38fabfa91dcb8defca729d6887be9ad4764d6ac
Yash Panchal <46398416+panchal-yash@users.noreply.github.com>
2023-06-23 18:14:22 +0530

    Update github-token-test.groovy

--- misc/github-token-test.groovy
+++ misc/github-token-test.groovy
@@ -43,15 +43,14 @@ pipeline {
                     cd package-testing
                     git config user.name "jenkins-pxc-cd"
                     git config user.email "it+jenkins-pxc-cd@percona.com"
-                    cat .git/config
                     OLD_REV=\$(cat VERSIONS | grep PS80_REV | cut -d '=' -f2- | sed 's/"//g')
                     OLD_VER=\$(cat VERSIONS | grep PS80_VER | cut -d '=' -f2- | sed 's/"//g')
                     sed -i "s/\$OLD_REV/${REVISION}/g" VERSIONS
                     sed -i "s/\$OLD_VER/${PS_RELEASE}/g" VERSIONS
                     git diff
                     git add -A
-                    git commit -m "add ${REVISION} and ${PS_RELEASE} for ps80 package testing VERSIONS file."
-                    git push -f 
+                    git commit -m "Autocommit: add ${REVISION} and ${PS_RELEASE} for ps80 package testing VERSIONS file."
+                    git push 
 
                     """
 
