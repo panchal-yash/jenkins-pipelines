b45cf27a05f40dce143d1d1589dfc8aef3845e66
Yash Panchal <46398416+panchal-yash@users.noreply.github.com>
2023-06-23 17:36:26 +0530

    Update github-token-test.groovy

--- misc/github-token-test.groovy
+++ misc/github-token-test.groovy
@@ -24,7 +24,11 @@ pipeline {
 
             
 
-            steps {              
+            steps {
+
+            REVISION = "SAMPLE_REVISION" 
+            PS_RELEASE="SAMPLE_VALUE"
+                
              script{
 
 
@@ -33,28 +37,17 @@ pipeline {
                     sh """
  
                     git clone https://jenkins-pxc-cd:$TOKEN@github.com/Percona-QA/package-testing.git
-
                     cd package-testing
-
-                    cat .git/config
-
-                    git checkout test
-
-                    cat .git/config
-
-                    git status
-
                     git config user.name "jenkins-pxc-cd"
                     git config user.email "it+jenkins-pxc-cd@percona.com"
-
                     cat .git/config
-
-                    echo "TESTING" > TESTFILE_JENKINS
-                    
+                    OLD_REV=$(cat VERSIONS | grep PS80_REV | cut -d '=' -f2- | sed 's/"//g')
+                    OLD_VER=$(cat VERSIONS | grep PS80_VER | cut -d '=' -f2- | sed 's/"//g')
+                    sed -i "s/$OLD_REV/${REVISION}/g" VERSIONS
+                    sed -i "s/$OLD_VER/${PS_RELEASE}/g" VERSIONS
+                    git diff
                     git add -A
-
-                    git commit -m "adding things automation"
-                    
+                    git commit -m "add ${REVISION} and ${PS_RELEASE} for ps80 package testing VERSIONS file."
                     git push -f 
 
                     """
