df51b2be8684568a56a5129042351bd95087c88c
panchal-yash <yash.panchal@percona.com>
2023-10-30 20:56:24 +0530

    Testing PXB8.1

---
+++ pxb/jenkins/pxb-package-testing-test.groovy
@@ -0,0 +1,236 @@
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+]) _
+
+product_action_playbooks = [
+    pxb80: [
+        install: 'pxb_80.yml',
+        upgrade: 'pxb_80_upgrade.yml',
+        upstream: 'pxb_80_upstream.yml',
+        tarball: 'pxb_80_tarball.yml',
+        kmip: 'pxb_80_kmip.yml'
+    ],
+    pxb24: [
+        install: 'pxb_24.yml',
+        upgrade: 'pxb_24_upgrade.yml',
+        upstream: 'pxb_24_upstream.yml',
+        tarball: 'pxb_24_tarball.yml'
+    ],
+    pxb81: [
+        install: 'pxb_81.yml',
+        upgrade: 'pxb_81_upgrade.yml',
+        upstream: 'pxb_81_upstream.yml',
+        tarball: 'pxb_81_tarball.yml',
+        kmip: 'pxb_81_kmip.yml'
+    ]
+]
+
+setup_centos_package_tests = { ->
+    sh '''
+        sudo yum install -y epel-release
+        sudo yum -y update
+        sudo yum install -y ansible
+    '''
+}
+
+setup_oracle8_package_tests = { ->
+    sh '''
+        sudo yum install -y epel-release
+        sudo yum -y update
+        sudo yum install -y ansible-2.9.27
+        sudo yum install -y tar
+    '''
+}
+
+setup_oracle9_package_tests = { ->
+    sh '''
+        sudo yum install -y epel-release
+        sudo yum -y update
+        sudo yum install -y ansible
+        sudo yum install -y tar
+    '''
+}
+
+setup_buster_package_tests = { ->
+    sh '''
+        sudo apt-get update
+        sudo apt-get install -y ansible
+    '''
+}
+
+setup_bullseye_package_tests = { ->
+    sh '''
+        sudo apt-get update
+        sudo apt-get install -y ansible
+    '''
+}
+
+setup_ubuntu_package_tests = { ->
+    sh '''
+        sudo apt-get update
+        sudo apt-get install -y software-properties-common
+        sudo apt-add-repository --yes --update ppa:ansible/ansible
+        sudo apt-get install -y ansible
+    '''
+}
+
+setup_ubuntu_jammy_package_tests = { ->
+    sh '''
+        sudo apt-get update
+        sudo apt-get install -y software-properties-common
+        sudo apt-get install -y python3 python3-pip
+        echo $PATH
+        python3 -m pip install --user ansible==2.9.27
+        ~/.local/bin/ansible --version
+        sudo ln -s ~/.local/bin/ansible /usr/bin/ansible
+        sudo ln -s ~/.local/bin/ansible-playbook /usr/bin/ansible-playbook
+    '''
+}
+
+
+node_setups = [
+    "min-buster-x64": setup_buster_package_tests,
+    "min-bullseye-x64": setup_bullseye_package_tests,
+    "min-bookworm-x64": setup_bullseye_package_tests,
+    "min-centos-7-x64": setup_centos_package_tests,
+    "min-ol-8-x64": setup_oracle8_package_tests,
+    "min-ol-9-x64": setup_oracle9_package_tests,
+    "min-bionic-x64": setup_ubuntu_package_tests,
+    "min-focal-x64": setup_ubuntu_package_tests,
+    "min-jammy-x64": setup_ubuntu_jammy_package_tests
+]
+
+void setup_package_tests() {
+    node_setups[params.node_to_test]()
+}
+
+void runPlaybook(String action_to_test) {
+    def playbook = product_action_playbooks[params.product_to_test][action_to_test]
+    def playbook_path = "package-testing/playbooks/${playbook}"
+    def git_repo = params.git_repo
+
+    sh """
+        git clone --branch pxb_version_update --depth 1 "${git_repo}"
+    """
+
+    setup_package_tests()
+
+    sh """
+        export install_repo="\${install_repo}"
+
+        ansible-playbook \
+        --connection=local \
+        --inventory 127.0.0.1, \
+        --limit 127.0.0.1 \
+        ${playbook_path}
+    """
+}
+
+
+pipeline {
+    agent none
+
+    parameters {
+        choice(
+            choices: ['pxb81', 'pxb80', 'pxb24'],
+            description: 'Choose the product version to test: PXB8.0 OR PXB2.4',
+            name: 'product_to_test'
+        )
+        choice(
+            choices: [
+                'min-centos-7-x64',
+                'min-ol-8-x64',
+                'min-ol-9-x64',
+                'min-bionic-x64',
+                'min-focal-x64',
+                'min-jammy-x64',
+                'min-buster-x64',
+                'min-bullseye-x64',
+                'min-bookworm-x64'
+            ],
+            description: 'Node to run tests',
+            name: 'node_to_test'
+        )
+        choice(
+            choices: ['testing', 'main', 'experimental'],
+            description: 'Choose the repo to install packages and run the tests',
+            name: 'install_repo'
+        )
+        string(
+            defaultValue: 'https://github.com/mohitj1988/package-testing.git',
+            description: '',
+            name: 'git_repo',
+            trim: false
+        )
+
+    }
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '15'))
+    }
+
+    stages {
+        stage("Prepare") {
+            steps {
+                script {
+                    currentBuild.displayName = "#${BUILD_NUMBER}-${params.product_to_test}-${params.install_repo}-${params.node_to_test}"
+                }
+            }
+        }
+
+        stage('Run parallel') {
+            parallel {
+                stage('Install') {
+                    agent {
+                        label params.node_to_test
+                    }
+
+                    steps {
+                        runPlaybook("install")
+                    }
+                }
+
+                stage('Upgrade') {
+                    agent {
+                        label params.node_to_test
+                    }
+
+                    steps {
+                        runPlaybook("upgrade")
+                    }
+                }
+
+                stage('Upstream') {
+                    agent {
+                        label params.node_to_test
+                    }
+
+                    steps {
+                        runPlaybook("upstream")
+                    }
+                }
+
+                stage('Tarball') {
+                    agent {
+                        label params.node_to_test
+                    }
+
+                    steps {
+                        runPlaybook("tarball")
+                    }
+                }
+
+                stage('Kmip') {
+                    agent {
+                        label params.node_to_test
+                    }
+
+                    steps {
+                        runPlaybook("kmip")
+                    }
+                }
+            }
+        }
+    }
+}
