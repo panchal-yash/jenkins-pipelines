d27cd4f23e53b5155595420b1822fae5659eee2c
panchal-yash <yash.panchal@percona.com>
2023-11-03 21:47:20 +0530

    PS80 Minitests add modification for conditional package tests trigger

--- ps/jenkins/percona-server-for-mysql-8.0.groovy
+++ ps/jenkins/percona-server-for-mysql-8.0.groovy
@@ -4,6 +4,8 @@ library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
+import groovy.transform.Field
+
 void installCli(String PLATFORM) {
     sh """
         set -o xtrace
@@ -125,7 +127,6 @@ def runPlaybook(def nodeName) {
     def install_mysql_shell = 'no'
 
     try {
-
         def playbook = "${ps80_install_pkg_minitests_playbook}"
         def playbook_path = "package-testing/playbooks/${playbook}"
 
@@ -147,24 +148,25 @@ def runPlaybook(def nodeName) {
         """
     } catch (Exception e) {
         slackNotify("${SLACKNOTIFY}", "#FF0000", "[${JOB_NAME}]: Mini Package Testing for ${nodeName} at ${BRANCH}  FAILED !!!")
+        mini_test_error="True"
     }
 }
 
-def package_tests_ps80() {
-    def arrayA = [  "min-buster-x64",
-                    "min-bullseye-x64",
-                    "min-bookworm-x64",
-                    "min-centos-7-x64",
-                    "min-ol-8-x64",
-                    "min-bionic-x64",
-                    "min-focal-x64",
-                    "min-amazon-2-x64",
-                    "min-jammy-x64",
-                    "min-ol-9-x64"     ]
+def minitestNodes = [  "min-buster-x64",
+                       "min-bullseye-x64",
+                       "min-bookworm-x64",
+                       "min-centos-7-x64",
+                       "min-ol-8-x64",
+                       "min-bionic-x64",
+                       "min-focal-x64",
+                       "min-amazon-2-x64",
+                       "min-jammy-x64",
+                       "min-ol-9-x64"     ]
 
+def package_tests_ps80(def nodes) {
     def stepsForParallel = [:]
-    for (int i = 0; i < arrayA.size(); i++) {
-        def nodeName = arrayA[i]
+    for (int i = 0; i < nodes.size(); i++) {
+        def nodeName = nodes[i]
         stepsForParallel[nodeName] = {
             stage("Minitest run on ${nodeName}") {
                 node(nodeName) {
@@ -178,6 +180,7 @@ def package_tests_ps80() {
 }
 
 def AWS_STASH_PATH
+@Field def mini_test_error = "False"
 
 pipeline {
     agent {
@@ -205,7 +208,7 @@ parameters {
             description: 'Repo component to push packages to',
             name: 'COMPONENT')
         choice(
-            choices: '#dev-server-qa\n#releases\n#releases-ci',
+            choices: '#releases\n#releases-ci',
             description: 'Channel for notifications',
             name: 'SLACKNOTIFY')
     }
@@ -216,7 +219,7 @@ parameters {
         timestamps ()
     }
     stages {
-/*
+
         stage('Create PS source tarball') {
             agent {
                label 'min-bionic-x64'
@@ -672,23 +675,81 @@ parameters {
                 }
             }
         }
-*/
-        stage("TESTING"){
-            steps{
-                echo "TESTING"
+        stage('Build docker containers') {
+            agent {
+                label 'min-bionic-x64'
             }
-        }
-
+            steps {
+                echo "====> Build docker container"
+                cleanUpWS()
+                installCli("deb")
+                sh '''
+                   sleep 900
+                '''
+                unstash 'properties'
+                sh '''
+                    PS_RELEASE=$(echo ${BRANCH} | sed 's/release-//g')
+                    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+                    sudo apt-get install -y docker.io
+                    sudo systemctl status docker
+                    sudo apt-get install -y qemu binfmt-support qemu-user-static
+                    sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+                    git clone https://github.com/percona/percona-docker
+                    cd percona-docker/percona-server-8.0
+                    sed -i "s/ENV PS_VERSION.*/ENV PS_VERSION ${PS_RELEASE}.${RPM_RELEASE}/g" Dockerfile
+                    sed -i "s/ENV PS_REPO .*/ENV PS_REPO testing/g" Dockerfile
+                    sed -i "s/ENV PS_VERSION.*/ENV PS_VERSION ${PS_RELEASE}.${RPM_RELEASE}/g" Dockerfile.aarch64
+                    sed -i "s/ENV PS_REPO .*/ENV PS_REPO testing/g" Dockerfile.aarch64
+                    sudo docker build -t perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE} .
+                    sudo docker build -t perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}-aarch64 -f Dockerfile.aarch64 .
+                    sudo docker images
+                 '''
+                 withCredentials([
+                     usernamePassword(credentialsId: 'hub.docker.com',
+                     passwordVariable: 'PASS',
+                     usernameVariable: 'USER'
+                     )]) {
+                 sh '''
+                     echo "${PASS}" | sudo docker login -u "${USER}" --password-stdin
+                     PS_RELEASE=$(echo ${BRANCH} | sed 's/release-//g')
+                     sudo docker tag perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE} perconalab/percona-server:${PS_RELEASE}
+                     sudo docker push perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}
+                     sudo docker push perconalab/percona-server:${PS_RELEASE}
+                     sudo docker tag perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}-aarch64 perconalab/percona-server:${PS_RELEASE}-aarch64
+                     sudo docker push perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}-aarch64
+                     sudo docker push perconalab/percona-server:${PS_RELEASE}-aarch64
+                 '''
+                 }
+                 sh '''
+                    PS_RELEASE=$(echo ${BRANCH} | sed 's/release-//g')
+                    sudo docker manifest create perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}-multi \
+                         perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE} \
+                         perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}-aarch64
+                    sudo docker manifest annotate perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}-multi perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}-aarch64 --os linux --arch arm64 --variant v8
+                    sudo docker manifest annotate perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}-multi perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE} --os linux --arch amd64
+                    sudo docker manifest inspect perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}-multi
+                '''
+                 withCredentials([
+                    usernamePassword(credentialsId: 'hub.docker.com',
+                    passwordVariable: 'PASS',
+                    usernameVariable: 'USER'
+                    )]) {
+                 sh '''
+                    PS_RELEASE=$(echo ${BRANCH} | sed 's/release-//g')
+                    echo "${PASS}" | sudo docker login -u "${USER}" --password-stdin
+                    PS_RELEASE=$(echo ${BRANCH} | sed 's/release-//g')
+                    sudo docker manifest push perconalab/percona-server:${PS_RELEASE}.${RPM_RELEASE}-multi
+                 '''
+                 }
+           }
+       }
     }
     post {
         success {
-            /*
             slackNotify("${SLACKNOTIFY}", "#00FF00", "[${JOB_NAME}]: build has been finished successfully for ${BRANCH} - [${BUILD_URL}]")
             slackNotify("${SLACKNOTIFY}", "#00FF00", "[${JOB_NAME}]: Triggering Builds for Package Testing for ${BRANCH} - [${BUILD_URL}]")
             unstash 'properties'
-            */
             script {
-            /*
                 currentBuild.description = "Built on ${BRANCH}; path to packages: ${COMPONENT}/${AWS_STASH_PATH}"
                 REVISION = sh(returnStdout: true, script: "grep REVISION test/percona-server-8.0.properties | awk -F '=' '{ print\$2 }'").trim()
                 PS_RELEASE = sh(returnStdout: true, script: "echo ${BRANCH} | sed 's/release-//g'").trim()
@@ -712,27 +773,29 @@ parameters {
 
                 """
                 }
-            */
                 echo "Start Minitests for PS"
                 
-                package_tests_ps80()
-                
-                echo "Trigger Package Testing Job for PS"
+                package_tests_ps80(minitestNodes)
 
-                build job: 'package-testing-ps80', propagate: false, wait: false, parameters: [string(name: 'product_to_test', value: 'ps80'),string(name: 'install_repo', value: "testing"),string(name: 'node_to_test', value: "all"),string(name: 'action_to_test', value: "all"),string(name: 'check_warnings', value: "yes"),string(name: 'install_mysql_shell', value: "no")]
-                
-                echo "Trigger PMM_PS Github Actions Workflow"
-                
-                withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'GITHUB_API_TOKEN')]) {
-                    sh """
-                        curl -i -v -X POST \
-                             -H "Accept: application/vnd.github.v3+json" \
-                             -H "Authorization: token ${GITHUB_API_TOKEN}" \
-                             "https://api.github.com/repos/Percona-Lab/qa-integration/actions/workflows/PMM_PS.yaml/dispatches" \
-                             -d '{"ref":"main","inputs":{"ps_version":"${PS_RELEASE}"}}'
-                    """
+                if("${mini_test_error}" == "True"){
+                    echo "NOT TRIGGERING PACKAGE TESTS AND INTEGRATION TESTS DUE TO MINITEST FAILURE !!"
+                }else{
+                    echo "Trigger Package Testing Job for PS"
+
+                    build job: 'package-testing-ps80', propagate: false, wait: false, parameters: [string(name: 'product_to_test', value: 'ps80'),string(name: 'install_repo', value: "testing"),string(name: 'node_to_test', value: "all"),string(name: 'action_to_test', value: "all"),string(name: 'check_warnings', value: "yes"),string(name: 'install_mysql_shell', value: "no")]
+                    
+                    echo "Trigger PMM_PS Github Actions Workflow"
+                    
+                    withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'GITHUB_API_TOKEN')]) {
+                        sh """
+                            curl -i -v -X POST \
+                                -H "Accept: application/vnd.github.v3+json" \
+                                -H "Authorization: token ${GITHUB_API_TOKEN}" \
+                                "https://api.github.com/repos/Percona-Lab/qa-integration/actions/workflows/PMM_PS.yaml/dispatches" \
+                                -d '{"ref":"main","inputs":{"ps_version":"${PS_RELEASE}"}}'
+                        """
+                    }
                 }
-
             }
             deleteDir()
         }
