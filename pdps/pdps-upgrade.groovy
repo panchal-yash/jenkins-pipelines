7c43b7d7e55c025a766bedfb09c75707745a4c28
panchal-yash <yash.panchal@percona.com>
2024-06-10 16:19:47 +0530

    Test pdps rhel8 and 9 arm

--- pdps/pdps-upgrade.groovy
+++ pdps/pdps-upgrade.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: "lib@master", retriever: modernSCM([
+library changelog: false, identifier: "lib@psmdb-package-tests-rhel-arm-support", retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/panchal-yash/jenkins-pipelines.git'
 ])
 
 pipeline {
