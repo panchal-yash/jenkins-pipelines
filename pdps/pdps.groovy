58a532389f53b037bd48806558136fe48c1104c5
panchal-yash <yash.panchal@percona.com>
2024-08-22 20:03:25 +0530

    add noble support

--- pdps/pdps.groovy
+++ pdps/pdps.groovy
@@ -6,7 +6,7 @@ library changelog: false, identifier: "lib@master", retriever: modernSCM([
 
 pipeline {
     agent {
-    label 'min-ol-8-x64'
+    label 'min-bookworm-x64'
     }
     environment {
       PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin';
@@ -161,4 +161,19 @@ pipeline {
             }
         }
     }
+}
+
+def installMolecule() {
+        sh """
+            echo "HELLO!!!!!!!!!!!"
+            sudo apt update -y
+            sudo apt install -y python3 python3-pip python3-dev python3-venv
+            python3 -m venv virtenv
+            . virtenv/bin/activate
+            python3 --version
+            python3 -m pip install --upgrade pip
+            python3 -m pip install --upgrade setuptools
+            python3 -m pip install --upgrade setuptools-rust
+            python3 -m pip install --upgrade PyYaml==5.3.1 molecule==3.3.0 testinfra pytest molecule-ec2==0.3 molecule[ansible] "ansible<10.0.0" "ansible-lint>=5.1.1,<6.0.0" boto3 boto
+        """
 }
